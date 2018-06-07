768d5cb82711b66b37ed2aa1a4bbe03d97ad9a54
chrisli8 <8chrisgli@gmail.com>
2018-06-07 12:49:40 -0700

    graph lines and gradient added

--- Flo/GraphView.swift
+++ Flo/GraphView.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 @IBDesignable class GraphView: UIView {
+    
+    private struct Constants {
+        static let cornerRadiusSize = CGSize(width: 8.0, height: 8.0)
+        static let margin: CGFloat = 20.0
+        static let topBorder: CGFloat = 60
+        static let bottomBorder: CGFloat = 50
+        static let colorAlpha: CGFloat = 0.3
+        static let circleDiameter: CGFloat = 5.0
+        
+    }
+    
+    //Weekly sample data
+    var graphPoints = [4, 2, 6, 4, 5, 8, 3]
 
     //set start and end colors for gradient so changable in storyboard
     @IBInspectable var startColor: UIColor = .red
@@ -16,6 +29,16 @@ import UIKit
     
     
     override func draw(_ rect: CGRect) {
+        
+        let width = rect.width
+        let height = rect.height
+        
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: .allCorners,
+                                cornerRadii: Constants.cornerRadiusSize)
+        
+        path.addClip()
+        
         //tell CG drawing functions which context
         let context = UIGraphicsGetCurrentContext()!
         let colors = [startColor.cgColor, endColor.cgColor]
@@ -38,6 +61,72 @@ import UIKit
                                    start: startPoint,
                                    end: endPoint,
                                    options: [])
+        
+        //calculate the x point
+        
+        let margin = Constants.margin
+        let graphWidth = width - margin * 2 - 4
+        let columnXPoint = {(column: Int) -> CGFloat in
+            //Calculate the gap between points
+            let spacing = graphWidth / CGFloat(self.graphPoints.count - 1)
+            return CGFloat(column) * spacing + margin + 2
+        }
+        
+        // calculate the y point
+        
+        let topBorder = Constants.topBorder
+        let bottomBorder = Constants.bottomBorder
+        let graphHeight = height - topBorder - bottomBorder
+        let maxValue = graphPoints.max()!
+        let columnYPoint = { (graphPoint: Int) -> CGFloat in
+            let y = CGFloat(graphPoint) / CGFloat(maxValue) * graphHeight
+            return graphHeight + topBorder - y // Flip the graph
+        }
+        
+        // draw the line graph
+        
+        UIColor.white.setFill()
+        UIColor.white.setStroke()
+        
+        // set up the points line
+        let graphPath = UIBezierPath()
+        
+        // go to start of line
+        graphPath.move(to: CGPoint(x: columnXPoint(0), y: columnYPoint(graphPoints[0])))
+        
+        // add points for each item in the graphPoints array
+        // at the correct (x, y) for the point
+        for i in 1..<graphPoints.count {
+            let nextPoint = CGPoint(x: columnXPoint(i), y: columnYPoint(graphPoints[i]))
+            graphPath.addLine(to: nextPoint)
+        }
+        
+        //create clipping path for the graph gradient
+        
+        //save the state of the context
+        //context.saveGState()
+        
+        // make copy of the path
+        let clippingPath = graphPath.copy() as! UIBezierPath
+        
+        //add lines to the copied path to complete the clip area
+        clippingPath.addLine(to: CGPoint(x: columnXPoint(graphPoints.count - 1), y: height))
+        clippingPath.addLine(to: CGPoint(x: columnXPoint(0), y: height))
+        clippingPath.close()
+        
+        //add the clipping path to the context
+        clippingPath.addClip()
+        
+        let hightestYPoint = columnYPoint(maxValue)
+        let graphStartPoint = CGPoint(x: margin, y: hightestYPoint)
+        let graphEndPoint = CGPoint(x: margin, y: bounds.height)
+        
+        context.drawLinearGradient(gradient, start: graphStartPoint, end: graphEndPoint, options:[])
+        //context.restoreGState()
+        
+        //draw the line on top of the clipped gradient
+        graphPath.lineWidth = 2.0
+        graphPath.stroke()
     }
     
 
