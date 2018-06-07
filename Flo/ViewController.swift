9665e39bd5900259a07c4b914c6b59be2b469399
chrisli8 <8chrisgli@gmail.com>
2018-06-07 15:47:28 -0700

    graph layout and basic functionality finished

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -33,6 +33,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setupGraphDisplay() {
+        let maxDayIndex = stackView.arrangedSubviews.count - 1
+        
+        //replace last day with today's actual data
+        graphView.graphPoints[graphView.graphPoints.count - 1] = counterView.counter
+        //indicate that graph needs to be redrawn
+        graphView.setNeedsDisplay()
+        maxLabel.text = "\(graphView.graphPoints.max()!)"
+        
+        //calculate average from graphPoints
+        let average = graphView.graphPoints.reduce(0, +) / graphView.graphPoints.count
+        averageWaterDrunk.text = "\(average)"
+        
+        //setup date formatter and calendar
+        let today = Date()
+        let calendar = Calendar.current
+        
+        let formatter = DateFormatter()
+        formatter.setLocalizedDateFormatFromTemplate("EEEEE")
+        
+        //setup the day name labels with correct days
+        for i in 0...maxDayIndex {
+            if let date = calendar.date(byAdding: .day, value: -i, to: today),
+                let label = stackView.arrangedSubviews[maxDayIndex - i] as? UILabel {
+                label.text = formatter.string(from: date)
+            }
+        }
+    }
+    
+    // MARK: actions
+    
     @IBAction func pushButtonPressed(_ button: PushButton) {
         if button.isAddButton {
             counterView.counter += 1
@@ -58,6 +89,7 @@ class ViewController: UIViewController {
                              completion: nil)
         } else {
             //show Graph
+            setupGraphDisplay()
             UIView.transition(from: counterView,
                               to: graphView,
                               duration: 1.0,
