0397dbafed22c683e1fbf6c6e6e970a903a42ac4
chrisli8 <8chrisgli@gmail.com>
2018-06-06 18:32:44 -0700

    part 2 start, flip animation added

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     //Counter outlets
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    
+    var isGraphViewShowing = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +37,29 @@ class ViewController: UIViewController {
             }
         }
         counterLabel.text = String(counterView.counter)
+        
+        if isGraphViewShowing {
+            counterViewTap(nil)
+        }
+    }
+    
+    @IBAction func counterViewTap(_ gesture: UITapGestureRecognizer?) {
+        if (isGraphViewShowing) {
+            //hide Graph
+            UIView.transition(from: graphView,
+                             to: counterView,
+                             duration: 1.0,
+                             options: [.transitionFlipFromLeft, .showHideTransitionViews],
+                             completion: nil)
+        } else {
+            //show Graph
+            UIView.transition(from: counterView,
+                              to: graphView,
+                              duration: 1.0,
+                              options: [.transitionFlipFromRight, .showHideTransitionViews],
+                              completion: nil)
+        }
+        isGraphViewShowing = !isGraphViewShowing
     }
 
 
